be6690f514e4f073e35314e1fab4d751e41867c4
Doniyor Doniyorov <ddonierov96@gmail.com>
2023-08-29 09:01:40 +0500

    end deploy

--- swagger.js
+++ swagger.js
@@ -4,7 +4,7 @@ const options = {
     definition: {
         openapi: '3.0.0',
         info: {
-            title: 'Your API',
+            title: 'Upload file documentation',
             version: '1.0.0',
             description: 'API documentation for your Node.js Express application',
         },
