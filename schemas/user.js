c66767b222cf5978208e4ce204193d1b055314dc
Doniyor Doniyorov <ddonierov96@gmail.com>
2023-09-02 19:14:58 +0500

    all

--- schemas/user.js
+++ schemas/user.js
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 
 const UserSchema = new Schema({
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     verify: { type: Boolean, default: false }
